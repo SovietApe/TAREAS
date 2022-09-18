a2a021a2a854a2553ae3da64ea9bdf5f8a0d6e63
Lautaro Alegria <107960984+SovietApe@users.noreply.github.com>
2022-09-18 20:05:25 -0300

    Add files via upload

---
+++ Reactjs/src/pages/NovedadesPage.js
@@ -0,0 +1,15 @@
+import react from 'react';
+
+const NovedadesPage = (porps) => {
+    return (
+        <section className='holder'>
+            <h2>Novedades</h2>
+            <h3>Titulos</h3>
+            <h4>Subtitulos</h4>
+            <p>Cuerpo</p>
+        </section>
+    );
+}
+
+export default NovedadesPage;
+
